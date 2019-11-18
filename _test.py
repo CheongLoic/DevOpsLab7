dc6c22bfc114ca3131002ce24f09c24d0e582346
CheongLoic <47603928+CheongLoic@users.noreply.github.com>
2019-11-18 16:28:12 +0100

    Add files via upload

---
+++ _test.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
+
+import test2
+
+#print(addition(1,1))
+
+
+def test_answer():
+    val = test2.addition(3,2)
+    assert val == 5
